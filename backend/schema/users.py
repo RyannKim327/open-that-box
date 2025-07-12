fa0a51c7a3c19f5fca051509019896ce0f2289cc
khian calderon <ckhianvictory@gmail.com>
2025-07-12 16:05:53 +0800

    Fixed Circular Import + Working Schema: @khianvictorycalderon

---
+++ backend/schema/users.py
@@ -0,0 +1,14 @@
+from app import app, db
+from datetime import datetime
+
+class Users(db.Model):
+    # Table for Users
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(200), nullable=False)
+    first_name = db.Column(db.String(200), nullable=False)
+    middle_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    badges = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.now())
