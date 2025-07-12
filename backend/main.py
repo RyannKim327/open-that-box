df7e0acf0b46acd57ba1a9dd64a5b2b37f772a54
khian calderon <ckhianvictory@gmail.com>
2025-07-12 17:36:00 +0800

    Completed working badge & achievement schema: @khianvictorycalderon

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,42 @@
 from app import app, db
 from flask import request
 from schema.users import Users
+from schema.achivements import Achivements
+from schema.badge import Badge
 from web.top_contrib import top_contributor
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    try:
-        content = Users(
-            username="khianvictory",
-            first_name="Khian Victory",
-            middle_name="Dela Pena",
-            last_name="Calderon",
-            role="Admin",
-            badges="",
-        )
-        db.session.add(content)
-        db.session.commit()
-    except Exception as error:
-        return f"Unable to inser: {error}"
 
-    return "Hello World"
+    def insert(content):
+        try :
+            db.session.add(content)
+            db.session.commit()
+        except Exception as error:
+            return f"Unable to insert: {error}"
+    
+    insert(Users(
+        user_username = "khianvictory", 
+        user_first_name = "Khian Victory",
+        user_middle_name = "Dela Pena",
+        user_last_name = "Calderon",
+        user_role = "Member",
+        user_badges = "70"
+    ))
+
+    insert(Badge(
+        badge_name = "Sa kaniya lang",
+        badge_description = "Ako ay para sa kaniya lamang",
+        badge_level_points = "[10, 10, 50, 20]",
+        badge_category = "Single"
+    ))
+
+    insert(Achivements(
+        user_id=1,
+        badge_id=1
+    ))
+
+    return "Hello World"    
 
 
 @app.route("/top-contributors")
--- backend/schema/badge.py
+++ backend/schema/badge.py
@@ -1,11 +1,10 @@
 from app import db
-from sqlalchemy.orm import Mapped, mapped_column
 
 class Badge(db.Model):
     __tablename__ = "tbl_badge"
 
-    badge_id: Mapped[int] = mapped_column(primary_key=True)
-    badge_name: Mapped[str] = mapped_column(unique=True)
-    badge_description: Mapped[str] = mapped_column()
-    badge_category: Mapped[str] = mapped_column()
-    badge_level_points: Mapped[int] = mapped_column()
+    badge_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    badge_name = db.Column(db.String(200), nullable=False)
+    badge_description = db.Column(db.Text, nullable=False)
+    badge_level_points = db.Column(db.String(200), default="")
+    badge_category = db.Column(db.String(50), nullable=False)
