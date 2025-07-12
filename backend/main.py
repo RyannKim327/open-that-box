c297f96a27616d72e91866da9180e05a028d1db8
RyannKim327 <rksesgundo123@gmail.com>
2025-07-12 14:34:22 +0800

    07-12-25 14:34 - @RyannKim327

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import database
-from flask import Flask
+from flask import Flask, request
+from web.top_contrib import top_contributor
 
 app = Flask(__name__)
 db = database.db(app)
@@ -8,6 +9,11 @@ db = database.db(app)
 @app.route("/", methods=["POST", "GET"])
 def index():
     return "Hello World"
+
+
+@app.router("/top-contributors")
+def top_contribs():
+    return top_contributor(request)
 
 
 if __name__ == "__main__":
