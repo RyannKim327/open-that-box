b9b845b2083f64fb5f5e1461e26177ed68fbebee
khian calderon <ckhianvictory@gmail.com>
2025-07-12 16:13:33 +0800

    Fixed: Users imported: @khianvictorycalderon

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,7 @@
 from flask import request
 from web.top_contrib import top_contributor
-from datetime import datetime
 from app import app, db
-
-# ------------------------------------------------------------------------------------------
-# Database Schema
-
-class Users(db.Model):
-    # Table for Users
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(200), nullable=False)
-    first_name = db.Column(db.String(200), nullable=False)
-    middle_name = db.Column(db.String(200), nullable=False)
-    last_name = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-    badges = db.Column(db.String(50))
-    created_at = db.Column(db.DateTime, default=datetime.now())
-
-# ------------------------------------------------------------------------------------------
+from schema.users import Users
 
 @app.route("/", methods=["POST", "GET"])
 def index():
