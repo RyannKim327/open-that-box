f26dc513f6f84a8091c67c6f065979ac01987c20
khian calderon <ckhianvictory@gmail.com>
2025-07-12 20:51:49 +0800

    Updated

---
+++ frontend/src/register_test.ts
@@ -0,0 +1,17 @@
+import { post } from "./utils/access.ts";
+
+(async () => {
+  const endpoint = "/register";
+  const data = {
+    // All of these must be present upon user registration, otherwise the server will reject the request
+    username: "a",
+    first_name: "f",
+    middle_name: "s",
+    last_name: "f",
+    email: "f",
+    password: "g",
+    confirm_password: ""
+  };
+  const response = await post(endpoint, data);
+  console.log(JSON.stringify(response));
+})();
